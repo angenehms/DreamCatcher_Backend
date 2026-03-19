3be97810b1f026ef3760c9f7cb29b954e3e9377a
angenehms <angenehms@gmail.com>
2026-03-20 02:52:04 +0900

    [DCB1] Chore : 프로젝트 생성 및 .gitignore 설정

---
+++ src/main/java/com/dreamcatcher/backend/DreamcatcherApplication.java
@@ -0,0 +1,13 @@
+package com.dreamcatcher.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DreamcatcherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DreamcatcherApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dreamcatcher/backend/DreamcatcherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dreamcatcher.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DreamcatcherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
