59af516ceaf1df37119b218fa157769f1f293446
angenehms <angenehms@gmail.com>
2026-03-24 03:51:07 +0900

    [DCB22] Feat : Member 및 Reservation 엔티티 조회를 위한 Repository 생성

---
+++ src/main/java/com/dreamcatcher/backend/reservation/domain/ReservationRepository.java
@@ -0,0 +1,6 @@
+package com.dreamcatcher.backend.reservation.domain;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ReservationRepository extends JpaRepository<Reservation, Long> {
+}
